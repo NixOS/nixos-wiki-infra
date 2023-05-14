eb9245d132652cb0ba3d81f5ef9e9c736935707a
Jörg Thalheim <joerg@thalheim.io>
2023-05-14 18:37:15 +0200

    more treefmt

--- terraform/modules/wiki/variables.tf
+++ terraform/modules/wiki/variables.tf
@@ -1,37 +1,37 @@
 variable "server_type" {
-  type = string
-  default = "cx21"
+  type        = string
+  default     = "cx21"
   description = "Hetzner cloud server type"
 }
 
 variable "server_location" {
-  type = string
-  default = "hel1"
+  type        = string
+  default     = "hel1"
   description = "Hetzner cloud server location"
 }
 
 variable "netlify_dns_zone" {
-  type = string
+  type        = string
   description = "Netlify DNS zone"
 }
 
 variable "nixos_vars_file" {
-  type = string
+  type        = string
   description = "File to write NixOS configuration variables to"
 }
 
 variable "nixos_flake_attr" {
-  type = string
+  type        = string
   description = "NixOS configuration flake attribute"
 }
 
 variable "domain" {
-  type = string
+  type        = string
   description = "Domain name"
 }
 
 variable "tags" {
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
   description = "Tags to add to the server"
 }
