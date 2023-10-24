d2e73d80ffefa8c1c9cd481cae110b281e26c793
Jörg Thalheim <joerg@thalheim.io>
2023-10-24 18:59:19 +0200

    fix nginx + firewall

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -2,6 +2,7 @@
   flake.nixosModules = {
     hcloud.imports = [
       inputs.srvos.nixosModules.server
+      inputs.srvos.nixosModules.mixins-nginx
       inputs.sops-nix.nixosModules.sops
       inputs.srvos.nixosModules.hardware-hetzner-cloud
       ./single-disk.nix
