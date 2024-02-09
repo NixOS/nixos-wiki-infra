e0f1dbc89e4e6882c2241421c78a362263ac4fbf
Julien Malka <julien@malka.sh>
2024-02-09 11:42:06 +0100

    admins: add julien's key

--- targets/admins/terraform.tf
+++ targets/admins/terraform.tf
@@ -13,5 +13,6 @@ module "wiki" {
   source = "../../modules/admins"
   ssh_keys = {
     mic92 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbBp2dH2X3dcU1zh+xW3ZsdYROKpJd3n13ssOP092qE joerg@turingmachine"
+    julien = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDMBW7rTtfZL9wtrpCVgariKdpN60/VeAzXkh9w3MwbO julien@enigma"
   }
 }
