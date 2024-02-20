51a349428f5374b71709ab5ee9ed118a91a66f72
Jörg Thalheim <joerg@thalheim.io>
2024-02-20 08:33:07 +0100

    allow prometheus to access metrics

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -7,6 +7,7 @@
       inputs.srvos.nixosModules.hardware-hetzner-cloud
       inputs.srvos.nixosModules.mixins-telegraf
       ./single-disk.nix
+      ./monitoring.nix
       {
         sops.age.keyFile = "/var/lib/secrets/age";
       }
