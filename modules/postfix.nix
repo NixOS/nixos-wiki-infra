e263cf33be9a7f34aa79534d948a9328eb274efa
Jörg Thalheim <joerg@thalheim.io>
2024-03-06 19:52:43 +0100

    postfix: disable dnssec

--- modules/postfix.nix
+++ modules/postfix.nix
@@ -25,7 +25,6 @@ in
     config = {
       smtp_tls_note_starttls_offer = "yes";
 
-      smtp_dns_support_level = "dnssec";
       smtp_tls_security_level = "dane";
 
       tls_medium_cipherlist = "AES128+EECDH:AES128+EDH";
