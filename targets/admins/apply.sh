e5703f798260807f259476b3a26b14017116d6e5
Jörg Thalheim <joerg@thalheim.io>
2024-04-01 15:24:51 +0200

    targets/admins: switch to tofu

--- targets/admins/apply.sh
+++ targets/admins/apply.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 rm -f .terraform.lock.hcl
-terraform init -backend-config="password=$GITLAB_TOKEN" -backend-config="username=$GITLAB_USER"
-terraform apply
+tofu init -backend-config="password=$GITLAB_TOKEN" -backend-config="username=$GITLAB_USER"
+tofu apply
 
