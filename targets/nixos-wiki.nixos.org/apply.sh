dc83e1dc89d79f8297c3ea5964c6af411292cd48
Jörg Thalheim <joerg@thalheim.io>
2024-04-08 07:28:06 +0200

    chdir in apply scripts

--- targets/nixos-wiki.nixos.org/apply.sh
+++ targets/nixos-wiki.nixos.org/apply.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd "$SCRIPT_DIR"
 rm -f .terraform.lock.hcl
 tofu init -backend-config="password=$GITLAB_TOKEN" -backend-config="username=$GITLAB_USER"
 tofu apply
