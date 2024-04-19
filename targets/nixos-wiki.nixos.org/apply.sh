2a4829a254e95cae023af0212630ece8deedf66a
Jörg Thalheim <joerg@thalheim.io>
2024-04-19 09:48:50 +0200

    run nixos tests before deploying

--- targets/nixos-wiki.nixos.org/apply.sh
+++ targets/nixos-wiki.nixos.org/apply.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 rm -f .terraform.lock.hcl
+nix build .#checks.x86_64-linux.test -L
 tofu init -backend-config="password=$GITLAB_TOKEN" -backend-config="username=$GITLAB_USER"
 tofu apply "$@"
