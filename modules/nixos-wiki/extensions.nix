3742886004bcfeb20ef78126269c386b2da042fe
Jörg Thalheim <joerg@thalheim.io>
2024-04-19 09:52:26 +0200

    enable quick instant commons extension
    fixes #45

--- modules/nixos-wiki/extensions.nix
+++ modules/nixos-wiki/extensions.nix
@@ -1,4 +1,5 @@
 { fetchzip }: {
   "MobileFrontend" = fetchzip { url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/MobileFrontend-REL1_41-7e9d1d4.tar.gz/MobileFrontend-REL1_41-7e9d1d4.tar.gz"; sha256 = "0qk38j2mj9mny2qrzc4y56ng06mks3isz8gk90rgaw6ikcxg5dmj"; };
   "DarkMode" = fetchzip { url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/DarkMode-REL1_41-b2fefd2.tar.gz/DarkMode-REL1_41-b2fefd2.tar.gz"; sha256 = "0v3jyd2da18mbf0kn4zafw7rpj8awsiljy9jfhabric9k09g57y3"; };
+  "QuickInstantCommons" = fetchzip { url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/QuickInstantCommons-REL1_41-2a29b3e.tar.gz/QuickInstantCommons-REL1_41-2a29b3e.tar.gz"; sha256 = "1maqrx9k2bikin6fqn920v9l5nxaijpk14m0g2wpipryxx727vni"; };
 }
