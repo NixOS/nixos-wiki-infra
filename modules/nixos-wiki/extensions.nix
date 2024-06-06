9a13742f4c4e9a477c95a3f6fcb085613034e3ab
Mic92 <96200+Mic92@users.noreply.github.com>
2024-06-06 09:29:07 +0000

    [create-pull-request] automated change

--- modules/nixos-wiki/extensions.nix
+++ modules/nixos-wiki/extensions.nix
@@ -1,5 +1,5 @@
 { fetchzip }: {
-  "MobileFrontend" = fetchzip { url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/MobileFrontend-REL1_41-975f3e9.tar.gz/MobileFrontend-REL1_41-975f3e9.tar.gz"; sha256 = "1wrlk3pa5zgznb3g6bbiqiyzmmd4bg9afrkl4gsmyqlyjdlgh098"; };
-  "DarkMode" = fetchzip { url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/DarkMode-REL1_41-ed0fbad.tar.gz/DarkMode-REL1_41-ed0fbad.tar.gz"; sha256 = "1m403p17nbpcdgkj7691qpwj8g0146dmink13ay01395hzc9f3js"; };
-  "QuickInstantCommons" = fetchzip { url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/QuickInstantCommons-REL1_41-2a29b3e.tar.gz/QuickInstantCommons-REL1_41-2a29b3e.tar.gz"; sha256 = "1maqrx9k2bikin6fqn920v9l5nxaijpk14m0g2wpipryxx727vni"; };
+  "MobileFrontend" = fetchzip { url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/MobileFrontend-REL1_41-9c76ddf.tar.gz/MobileFrontend-REL1_41-9c76ddf.tar.gz"; sha256 = "0vghqw2z13p3wphqi6ar8nwf3iy4knkl6hrbq2qpqa2bybk9fvhy"; };
+  "DarkMode" = fetchzip { url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/DarkMode-REL1_41-d456330.tar.gz/DarkMode-REL1_41-d456330.tar.gz"; sha256 = "0rj7yd72ik4hkbpfp682z158nrn9lqv841jgkjand1j4z64raj57"; };
+  "QuickInstantCommons" = fetchzip { url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/QuickInstantCommons-REL1_41-0c2fd1b.tar.gz/QuickInstantCommons-REL1_41-0c2fd1b.tar.gz"; sha256 = "0ldw9a53hkxff1bm7likbnrddcriag824d0sr49sabrxa5flygln"; };
 }
