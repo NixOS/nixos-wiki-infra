4142b90e9e20b7c0fdc704bd5ffec6b663e788b0
Jörg Thalheim <joerg@thalheim.io>
2024-07-01 15:49:23 +0200

    Retry a second time when a deployment fails once

--- targets/nixos-wiki.nixos.org/deploy.sh
+++ targets/nixos-wiki.nixos.org/deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 nix build .#checks.x86_64-linux.test -L
-nixos-rebuild switch --flake .#nixos-wiki-nixos-org --target-host root@wiki.nixos.org
+if ! nixos-rebuild switch --flake .#nixos-wiki-nixos-org --target-host root@wiki.nixos.org; then
+  nixos-rebuild switch --flake .#nixos-wiki-nixos-org --target-host root@wiki.nixos.org
+fi
