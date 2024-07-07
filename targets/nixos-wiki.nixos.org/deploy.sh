49abb6930dacd252ffe12d49309f071064bf9891
Jörg Thalheim <joerg@thalheim.io>
2024-07-07 18:10:51 +0200

    deploy: build nixos closure in parallel to nixos test

--- targets/nixos-wiki.nixos.org/deploy.sh
+++ targets/nixos-wiki.nixos.org/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-nix build .#checks.x86_64-linux.test -L
+nix build .#checks.x86_64-linux.test .#nixosConfigurations.nixos-wiki-nixos-org.config.system.build.toplevel -L
 if ! nixos-rebuild switch --flake .#nixos-wiki-nixos-org --target-host root@wiki.nixos.org; then
   nixos-rebuild switch --flake .#nixos-wiki-nixos-org --target-host root@wiki.nixos.org
 fi
