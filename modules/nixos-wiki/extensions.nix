3e9437d42ef6b31a8f566bd143e362a9d507d0e5
Jörg Thalheim <joerg@thalheim.io>
2024-07-20 20:29:12 +0200

    upload ExternalData extension

--- modules/nixos-wiki/extensions.nix
+++ modules/nixos-wiki/extensions.nix
@@ -4,4 +4,5 @@
   "QuickInstantCommons" = fetchzip { url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/QuickInstantCommons-REL1_42-3e6a069.tar.gz/QuickInstantCommons-REL1_42-3e6a069.tar.gz"; hash = "sha256-U7mNjhr0kI46gWForiUBKXQEYSuvME8+YVwMOVpuhm0="; };
   "Translate" = fetchzip { url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/Translate-REL1_42-0e93dab.tar.gz/Translate-REL1_42-0e93dab.tar.gz"; hash = "sha256-bZUm9PI9+GSDQ+rPf+tLhD3ayDkQsCY0Kcze//jr2L8="; };
   "UniversalLanguageSelector" = fetchzip { url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/UniversalLanguageSelector-REL1_42-87e9f44.tar.gz/UniversalLanguageSelector-REL1_42-87e9f44.tar.gz"; hash = "sha256-Ht3P5hNsXO12MCoeaqpbBcrFy/6V/Kj0ipcPsp9Vhi8="; };
+  "ExternalData" = fetchzip { url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/ExternalData-REL1_42-8c04ca8.tar.gz/ExternalData-REL1_42-8c04ca8.tar.gz"; hash = "sha256-u+DMWmbPjIkoP7644FQVTC83VSbQ1dgyYdDItowlI14="; };
 }
