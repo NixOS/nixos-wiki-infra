1d1135bb24f6d12db935e7a3438624edbcbacc49
Dionysis Grigoropoulos <dgrig@erethon.com>
2025-09-13 02:09:49 +0300

    monitoring: Enable node exporter

--- modules/monitoring.nix
+++ modules/monitoring.nix
@@ -6,4 +6,9 @@
   networking.firewall.extraStopCommands = ''
     ip6tables -D nixos-fw -p tcp --source 2a01:4f8:1c1a:37b2::1/128 --dport 9273 -j nixos-fw-accept || true
   '';
+
+  services.prometheus.exporters.node = {
+    enable = true;
+    openFirewall = true;
+  };
 }
