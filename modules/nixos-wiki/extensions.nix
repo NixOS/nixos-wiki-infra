edf6d99243bb512e8bf82b8f7ac0f99a3c5f90fe
Jörg Thalheim <joerg@thalheim.io>
2025-09-14 13:10:30 +0200

    fix updating extensions

--- modules/nixos-wiki/extensions.nix
+++ modules/nixos-wiki/extensions.nix
@@ -1,8 +1,8 @@
 { fetchzip }:
 {
   "MobileFrontend" = fetchzip {
-    url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/MobileFrontend-REL1_44-9d4d9f5.tar.gz/MobileFrontend-REL1_44-9d4d9f5.tar.gz";
-    hash = "sha256-XxhSGK9BE4omSe/VkHlSxZkFyaxFdfa2cYouRb3Tj14=";
+    url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/MobileFrontend-REL1_44-b7eaef9.tar.gz/MobileFrontend-REL1_44-b7eaef9.tar.gz";
+    hash = "sha256-T45uEDirjkcCUF1HmdVSN5uvtDRP/utdwpAGWwuvdaA=";
   };
   "DarkMode" = fetchzip {
     url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/DarkMode-REL1_44-4b937c9.tar.gz/DarkMode-REL1_44-4b937c9.tar.gz";
@@ -13,12 +13,12 @@
     hash = "sha256-wqa4VbjXnF59e/DRRkUX+nd45l+PLxTCMQXv5vBZ0BM=";
   };
   "Translate" = fetchzip {
-    url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/Translate-REL1_44-7f00900.tar.gz/Translate-REL1_44-7f00900.tar.gz";
-    hash = "sha256-8eEAQFZ01dkOSZNM8TNTtdTsrWuPhsPitx2IVXsNhpk=";
+    url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/Translate-REL1_44-3db4d9b.tar.gz/Translate-REL1_44-3db4d9b.tar.gz";
+    hash = "sha256-Y7hdJbn9BJZOPdBr1OTVvWo9Lfg3+qm8l+q8RsWmnFw=";
   };
   "UniversalLanguageSelector" = fetchzip {
-    url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/UniversalLanguageSelector-REL1_44-483b629.tar.gz/UniversalLanguageSelector-REL1_44-483b629.tar.gz";
-    hash = "sha256-cAL6984mtS3WVPLOOjaSlUbavU4ppCn4+tAaPgOZtUU=";
+    url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/UniversalLanguageSelector-REL1_44-f2c8d76.tar.gz/UniversalLanguageSelector-REL1_44-f2c8d76.tar.gz";
+    hash = "sha256-0daT/WPaIiMSkdtOLw0mEzKcwewGSWZbX+pBLNQIIVk=";
   };
   "Description2" = fetchzip {
     url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/Description2-REL1_44-defa8c4.tar.gz/Description2-REL1_44-defa8c4.tar.gz";
