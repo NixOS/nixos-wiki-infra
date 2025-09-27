f7fe77247237d20b10fc6472f2aedf6457785f22
Mic92 <96200+Mic92@users.noreply.github.com>
2025-09-27 03:07:51 +0000

    [create-pull-request] automated change

--- modules/nixos-wiki/extensions.nix
+++ modules/nixos-wiki/extensions.nix
@@ -1,8 +1,8 @@
 { fetchzip }:
 {
   "MobileFrontend" = fetchzip {
-    url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/MobileFrontend-REL1_44-086a79a.tar.gz/MobileFrontend-REL1_44-086a79a.tar.gz";
-    hash = "sha256-SFeEYqBPGOZ+I1h3qGdMQWkntI2TX1DJATsvJXJ63yE=";
+    url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/MobileFrontend-REL1_44-c75c95d.tar.gz/MobileFrontend-REL1_44-c75c95d.tar.gz";
+    hash = "sha256-YcRp3ApyB4lYYuGE9MJsf51GCfMC5EX2GMYZrc8kAck=";
   };
   "DarkMode" = fetchzip {
     url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/DarkMode-REL1_44-1561924.tar.gz/DarkMode-REL1_44-1561924.tar.gz";
@@ -13,12 +13,12 @@
     hash = "sha256-wqa4VbjXnF59e/DRRkUX+nd45l+PLxTCMQXv5vBZ0BM=";
   };
   "Translate" = fetchzip {
-    url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/Translate-REL1_44-266563a.tar.gz/Translate-REL1_44-266563a.tar.gz";
-    hash = "sha256-V5ukpD7dXBF5GvUAjJOZ2k4fC+jqOLg8LCTyGfJmsag=";
+    url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/Translate-REL1_44-bc50f3a.tar.gz/Translate-REL1_44-bc50f3a.tar.gz";
+    hash = "sha256-7/CTJ/6eSHO40mEHtRuLcH0qVLdZU8kIRSwf4D1WcN8=";
   };
   "UniversalLanguageSelector" = fetchzip {
-    url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/UniversalLanguageSelector-REL1_44-f03d369.tar.gz/UniversalLanguageSelector-REL1_44-f03d369.tar.gz";
-    hash = "sha256-pi+VQYpTvRIvYucB0Etlo7IsjeU+n2HpH5ZShgGFNuo=";
+    url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/UniversalLanguageSelector-REL1_44-44fd925.tar.gz/UniversalLanguageSelector-REL1_44-44fd925.tar.gz";
+    hash = "sha256-cvD/Nlq+7w9vKCQrfq7Dq6OlUHD9lOkTFxO8Ea9CA24=";
   };
   "Description2" = fetchzip {
     url = "https://github.com/NixOS/nixos-wiki-infra/releases/download/Description2-REL1_44-defa8c4.tar.gz/Description2-REL1_44-defa8c4.tar.gz";
